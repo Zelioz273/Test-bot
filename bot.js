d2bfb4125a509ef98898609846e67fc42ce78271
Zelioz273 <41243839+Zelioz273@users.noreply.github.com>
2018-07-18 15:52:21 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -73,12 +73,9 @@ client.on('message', message => {
 		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 	}
 
-	try {
+	
 		 command.execute(message, args);
-	}
+	
 
-	catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-		}	
+	
 });
