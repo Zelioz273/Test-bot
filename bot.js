9089f9e1b281a6f0884995e7ab568fb6f7bc8e51
Zelioz273 <41243839+Zelioz273@users.noreply.github.com>
2018-07-18 15:37:55 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on("message", message => {
   // The list of if/else is replaced with those simple 2 lines:
   try {
     let commandFile = require(`./commands/${command}.js`);
-    commandFile.runmessage);
+    commandfile.execute(message, args);
   } catch (err) {
     console.error(err);
   }
